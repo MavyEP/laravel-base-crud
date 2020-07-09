df59eeb41ba83ca98bde4a44c754ec4f32a8e08e
Edoardo Piergentili <epiergentili@outlook.it>
2020-07-09 19:38:01 +0200

    @2

---
+++ database/seeds/StudentsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+use App\Student;
+
+class StudentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < ; $i++) {
+          // code...
+        }
+    }
+}
